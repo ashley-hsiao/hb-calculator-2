b940bbb6e5f560f07ec37814905cc5342c60f69c
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 15:59:10 -0700

    Added while loop, raw input, and fixed if/else statements

--- calculator.py
+++ calculator.py
@@ -8,41 +8,46 @@ calculator program yourself in this file.
 from arithmetic import *
 
 # Your code goes here
-input = "q"
 
-tokens = input.split(" ")
+while True:
+    input = raw_input(">")
+    tokens = input.split(" ")
 
-if tokens[0] == "+": 
-    output = add(int(tokens[1]), int(tokens[2]))
-    print output
+    if tokens[0] == "q": 
+        quit()
 
-if tokens[0] == "-": 
-    output = subtract(int(tokens[1]), int(tokens[2]))
-    print output
-
-if tokens[0] == "*": 
-    output = multiply(int(tokens[1]), int(tokens[2]))
-    print output
-
-if tokens[0] == "/": 
-    output = divide(int(tokens[1]), int(tokens[2]))
-    print output
-
-if tokens[0] == "square": 
-    output = square(int(tokens[1]))
-    print output
-
-if tokens[0] == "cube": 
-    output = cube(int(tokens[1]))
-    print output    
-
-if tokens[0] == "pow": 
-    output = pow(int(tokens[1]), int(tokens[2]))
-    print output
-
-if tokens[0] == "mod": 
-    output = mod(int(tokens[1]), int(tokens[2]))
-    print output    
-
-if tokens[0] == "q": 
-    quit()
+    else:
+        if tokens[0] == "+": 
+            output = add(int(tokens[1]), int(tokens[2]))
+            print output
+    
+        elif tokens[0] == "-": 
+            output = subtract(int(tokens[1]), int(tokens[2]))
+            print output
+    
+        elif tokens[0] == "*": 
+            output = multiply(int(tokens[1]), int(tokens[2]))
+            print output
+    
+        elif tokens[0] == "/": 
+            output = divide(int(tokens[1]), int(tokens[2]))
+            print output
+    
+        elif tokens[0] == "square": 
+            output = square(int(tokens[1]))
+            print output
+    
+        elif tokens[0] == "cube": 
+            output = cube(int(tokens[1]))
+            print output    
+    
+        elif tokens[0] == "pow": 
+            output = pow(int(tokens[1]), int(tokens[2]))
+            print output
+    
+        elif tokens[0] == "mod": 
+            output = mod(int(tokens[1]), int(tokens[2]))
+            print output    
+    
+        else:
+            print "That is not a valid input, try again."
